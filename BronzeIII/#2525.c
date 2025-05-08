cfe2ee663278f3cb85df9cc8e370399e400edf9f
symarieChoi <symarie.choi@gmail.com>
2025-05-08 18:07:39 +0900

    2525번 해결

---
+++ BronzeIII/#2525.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int H, M; // 요리 시작 시간
+    scanf("%d %d", &H, &M);
+
+    int T; // 요리하는 데 걸리는 시간(분분)
+    scanf("%d", &T);
+
+    H += T / 60;
+    M += T % 60;
+
+    if (M >= 60)
+    {
+        H += M / 60;
+        M = M % 60;
+    }
+    if (H >= 24)
+    {
+        H %= 24;
+    }
+
+    printf("%d %d", H, M);
+
+    return 0;
+}
